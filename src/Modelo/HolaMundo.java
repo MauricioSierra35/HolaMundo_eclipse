9c547bf9999c8389f3cacbdd7a525dcb8432a857
MauricioSierra35 <santimauri3.4.1@gmail.com>
2024-08-28 17:45:01 -0500

    Se añadieron nuevos archivos

---
+++ src/Modelo/HolaMundo.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Esto solo imprimirá "Hola mundo" por consola
+		
+		System.out.println("Hola Mundo!");
+
+	}
+
+}
